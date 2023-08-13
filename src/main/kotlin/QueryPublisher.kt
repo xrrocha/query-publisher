21c2e31c9153763123245a49680fa3ec467f376a
Ricardo Rocha <ricardo@xrrocha.net>
2023-08-13 15:20:17 -0500

    Pass query parameters to route handlers

--- src/main/kotlin/QueryPublisher.kt
+++ src/main/kotlin/QueryPublisher.kt
@@ -30,6 +30,7 @@ class QueryPublisher(
                     staticFiles.directory = assetsDirectory.absolutePath
                     staticFiles.location = EXTERNAL
                 }
+                // TODO Add 404 handler
             }
             .apply {
                 get("/") { ctx -> ctx.html(pageHeader.htmlContent) }
@@ -45,7 +46,13 @@ class QueryPublisher(
                                 ${pageHeader.htmlContent}
                                 <hr>
                                 <br>
-                                ${route.run(connection, emptyMap()/*ctx.queryParamMap()*/)}
+                                ${
+                                route.run(
+                                    path,
+                                    connection,
+                                    // TODO Allow for multivalued params?
+                                    ctx.queryParamMap().mapValues { (_, values) -> values.firstOrNull() })
+                            }
                             </body>
                             </html>
                             """.trimIndent()
@@ -118,18 +125,63 @@ class Database(
             }
 }
 
-class Route(private val sql: String, private val template: String) {
-    fun run(connection: Connection, params: Map<String, Any?>) =
-        generateSequence(connection.createStatement().executeQuery(sql)) { it }
-            .takeWhile { it.next() }
-            .map { rs ->
-                (1..rs.metaData.columnCount).associate { colIdx ->
-                    rs.metaData.getColumnLabel(colIdx).lowercase() to rs.getObject(colIdx)
+class Route(sql: String, private val template: String) {
+    companion object {
+        private val ParameterNameRE = """:[_\p{IsLatin}][_\p{IsLatin}\d]+""".toRegex()
+
+        private val classMapper = mapOf<String, (String) -> Any?>(
+            "java.lang.Long" to java.lang.Long::parseLong,
+            "java.lang.Short" to java.lang.Short::parseShort,
+            "java.lang.Integer" to java.lang.Integer::parseInt,
+            "java.lang.Float" to java.lang.Float::parseFloat,
+            "java.lang.Double" to java.lang.Double::parseDouble,
+            "java.sql.Date" to java.sql.Date::valueOf,
+        )
+    }
+
+    private val paramSql = ParameterNameRE.replace(sql.trim(), "?")
+    private val paramNames = ParameterNameRE
+        .findAll(sql)
+        .map(MatchResult::value)
+        .withIndex()
+        .map { (index, value) -> index + 1 to value.substring(1) }
+        .toList()
+
+    fun run(path: String, connection: Connection, params: Map<String, String?>) =
+        generateSequence(
+            connection
+                .prepareStatement(paramSql)
+                .also { ps ->
+                    // TODO Cache prepared statement metadata
+                    paramNames.forEach { (index, name) ->
+                        val paramValue = params[name]?.let { value ->
+                            val converter = classMapper[ps.metaData.getColumnClassName(index)] ?: { it }
+                            converter(value)
+                        }
+                        if (paramValue != null) {
+                            ps.setObject(index, paramValue)
+                        } else {
+                            ps.setNull(index, ps.metaData.getColumnType(index))
+                        }
+                    }
+                }
+                .executeQuery()
+                .let { rs ->
+                    val colNames = (1..rs.metaData.columnCount)
+                        .map { index -> rs.metaData.getColumnLabel(index) }
+                    Pair(rs, colNames)
+                }
+        ) { it }
+            .takeWhile { (rs, _) -> rs.next() }
+            .map { (rs, colNames) ->
+                colNames.associate { colName ->
+                    colName.lowercase() to rs.getObject(colName)
                 }
             }
             .toList()
             .let { rows ->
-                val model = params + mapOf("rows" to rows)
-                Pug4J.render(StringReader(template), "", model)!!
+                val model = params + Pair("rows", rows)
+                // TODO Compile template at startup
+                Pug4J.render(StringReader(template), path, model)!!
             }
 }
